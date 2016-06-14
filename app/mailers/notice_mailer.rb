533a589fe076d41186e04d5c474e845f6dba68a7
Yusuke Hata <e106127y@gmail.com>
2016-06-15 01:39:38 +0900

    HTMLメールの内容をわかりやすくした。

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,5 +1,5 @@
 class NoticeMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "lodge@rarara.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -12,7 +12,7 @@ class NoticeMailer < ActionMailer::Base
     @article = article
     mail(
       to: User.pluck(:email),
-      subject: '[Lodge]' + @article.title
+      subject: '[Lodge] [New] ' + @article.title
     )
   end
 end
