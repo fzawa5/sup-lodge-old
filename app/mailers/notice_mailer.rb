5d526ec99f2f3681bda22466ba9bf57ab37bf555
Yusuke Hata <e106127y@gmail.com>
2016-06-15 22:09:02 +0900

    メール通知の送信元を環境変数 MAIL_SENDER のアドレスになるように変更した。

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,5 +1,5 @@
 class NoticeMailer < ActionMailer::Base
-  default from: "lodge@rarara.com"
+  default from: ENV['MAIL_SENDER']
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
